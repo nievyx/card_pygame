474fe1b28436b7e237655f74d4d13748f6bf1d29
Niamh <niamh.elizabeth@mail.com>
2026-03-26 19:58:22 +0000

    started button class in button.py

---
+++ button.py
@@ -0,0 +1,16 @@
+class Button:
+    def __int__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self, screen, outline=None):
+        if outline:
+            pygame.draw.rect(screen, outline, (self.x-2, self.y-2, self.width+4, self.height+4), 0)
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height), 0)
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 60)
+            text = font.render(self.text, 1, (0, 0 , 0 ))
