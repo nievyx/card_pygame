c913d62bd424f5f01c89a8e5a69c39ab2fffa777
Niamh <niamh.elizabeth@mail.com>
2026-03-30 23:03:32 +0100

    monster class created, v basic

---
+++ src/game/monster.py
@@ -0,0 +1,20 @@
+class Monster:
+    def __init__(self, name, hp, mp, energy, strength ):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.strength = strength # Strength of physical damage
+        self.energy = energy # Points needed for monsters to physical attack
+
+        self.max_hp = self.hp
+        self.max_mp = self.mp
+
+    def is_alive(self):
+        return self.hp > 0
+
+    def attack(self):
+        pass
+
+    def defense(self):
+        pass
+
