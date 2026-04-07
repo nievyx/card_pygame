12e2a2e572f1f8b358362468cca97ffb5ed59546
Niamh <niamh.elizabeth@mail.com>
2026-04-07 20:38:32 +0100

    data/monster.py now has current up to date monster data, however unused for now

---
+++ src/data/monsters.py
@@ -0,0 +1,35 @@
+"""
+This file is currently completely unused atm but will be used atm soon.
+"""
+from src.game.monster import Monster
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MONSTER_DIR = os.path.abspath(os.path.join(BASE_DIR, 'assets', 'monsters'))
+
+chimera = Monster(
+            name='Chimera',
+            image=MONSTER_DIR + '/chimera.png',
+            hp=12,
+            mp=8,
+            energy=5,
+            strength=4
+)
+demon = Monster(
+        name='Demon',
+        image=MONSTER_DIR + '/demon.png',
+        hp=15,
+        mp=4,
+        energy=8,
+        strength=9
+)
+calm = Monster(
+        name='Calm',
+        image=MONSTER_DIR + '/calm.png',
+        hp=10,
+        mp=6,
+        energy=6,
+        strength=3
+)
+
+monster_pool = [chimera, demon, calm]  # TODO: add auto list creation in class
