0b0165a1a39912af8cb5db631226d17c95bfec37
Nievyx <niamh.elizabeth@mail.com>
2026-05-08 12:40:21 +0100

    added funciton to deal cards viusally

--- src/ui/cards/card_renderer.py
+++ src/ui/cards/card_renderer.py
@@ -13,6 +13,33 @@ class CardRenderer:
 
         self.card_width, self.card_height = 118, 250
 
+        self.anim_positions = {}
+        self.deck_pos = (self.screen.get_width() // 2, self.screen.get_height() // 2)
+        self.animation_speed = 0.18
+
+    def get_dealt_cards(self, monster, target_rect):#TODO add typing
+        if monster not in self.anim_positions:
+            self.anim_positions[monster] = [
+                self.deck_pos[0],
+                self.deck_pos[1]
+            ]
+
+            pos = self.anim_positions[monster]
+
+            pos[0] += (target_rect.x - pos[0]) * self.animation_speed
+            pos[1] += (target_rect.y - pos[0]) * self.animation_speed
+
+            if abs(pos[0] - target_rect.x) < 1:
+                pos[0] = target_rect.x
+            if abs(pos[1] - target_rect.y) < 1:
+                pos[1] = target_rect.y
+
+            dealt_rect = target_rect.copy()
+            dealt_rect.x = round(pos[0])
+            dealt_rect.y = round(pos[1])
+            return dealt_rect
+        return None
+
     # TODO: Cache frames
     def get_card_frame(self, monster) -> pygame.Surface:
         """Checks monsters rarity attribute and returns card frame"""
